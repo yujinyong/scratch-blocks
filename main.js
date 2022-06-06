07d3f56a3257dd2984ef84c1f0fd91ed552e63d2
进勇 <927945860@qq.com>
2022-06-06 14:48:06 +0800

    Build for $(git log --pretty=format:%H -n1)

--- main.js
+++ main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("// intentionally left empty\n\n\n//# sourceURL=webpack:///./shim/gh-pages.js?");
+eval("// intentionally left empty\r\n\n\n//# sourceURL=webpack:///./shim/gh-pages.js?");
 
 /***/ })
 
